cde0b772c32ac4c52b50e8f7a5d5c9da9841261d
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-03-29 15:51:55 -0700

    custome types

--- Ch02/02_04/src/model.ts
+++ Ch02/02_04/src/model.ts
@@ -2,7 +2,18 @@ let displayName: string = "Jess's standing desk";
 let inventoryType: string = "furniture";
 let trackingNumber: string = "FD123455";
 let createDate: Date = new Date();
-let originalCost: any = 425;
+
+// union types 
+type Cost = number | string
+let originalCost: Cost;
+
+if(typeof originalCost === "number"){
+    let cost : number = originalCost
+
+}else{
+    let x = originalCost
+}
+
 originalCost = "A LOT of money!";
 
 enum InventoryItemType {
